c5fc29f61ce781ab7a044d0cf53746b20acfcd68
Mohd-Bilal <bilaljaleel123@gmail.com>
2018-01-03 19:24:38 +0530

    vatt push

---
+++ Renegade/app/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    //
+    public function user(){
+        return $this->belongsTo('App\User','author_id');
+
+    }
+    public function likes(){
+        return $this->hasMany('App\Like');
+
+    }
+    public function tags(){
+        return $this->hasMany('App\Tag');
+    }
+    
+
+
+    
+}
---
+++ Renegade/app/User.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+class User extends \TCG\Voyager\Models\User
+{
+    use Notifiable;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+    use \Illuminate\Auth\Authenticatable;
+    public function posts(){
+        return $this->hasMany('App\Post');
+
+    }
+    public function likes(){
+        return $this->hasMany('App\Like');
+
+    }
+    public function messages(){
+        return $this->hasMany('App\Message');
+
+    }
+
+
+}
